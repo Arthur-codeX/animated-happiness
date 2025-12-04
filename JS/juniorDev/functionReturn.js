87cc1a3d9b265dda1684517d853023a4a2fa4329
Arthur-codeX <daniel.karanja239@gmail.com>
2025-12-04 09:19:31 +0300

    completed scope

--- JS/juniorDev/functionReturn.js
+++ JS/juniorDev/functionReturn.js
@@ -65,3 +65,89 @@ function ageMessage(age) {
 
   alert(`${ageNumber} You are a fossil`);
 }
+
+function justReturn() {
+  console.log("My function was called");
+  return "ths is ccool";
+}
+
+// let x = justReturn();
+// console.log(`x is =${x} its types is ${typeof x}`);
+
+/*
+   1.Forget to have return stament
+   2.Empty Return statement
+   3.Return null :check
+   4.Return any number:check
+   5.Return any string you want:check
+*/
+
+/*
+  1:calculates area of circle
+  2:calulate permiter of the circle
+*/
+
+/*
+ you can assign a variable to function call
+*/
+function areaCirle(radius) {
+  let ans = radius * radius * 3.142;
+  return ans;
+}
+
+//Assign a function to a variable
+let myVar = areaCirle(11);
+console.log(`my var is, ${myVar}`);
+console.log("type of myvar ", typeof myVar);
+//Assigna function to a variable
+let myVar2 = areaCirle;
+console.log(`myVar2= `, myVar2);
+console.log("myVar2 type is= ", typeof myVar2);
+console.log(`function areaCircle=`, areaCirle);
+console.log(`areCircle type=`, typeof areaCirle);
+
+let myVar3 = myVar2(11);
+console.log(`my var3 is, ${myVar3}`);
+console.log("type of myVar3 ", typeof myVar3);
+/*
+  For all variables print the types
+  1.create a variable and assign it
+  to the function call of areCircle
+   with any radius(number)
+   Console the variable check what you get
+  2.create a variable assign the function
+  areaCircle.
+   console.log(areCircle)
+   console.log(theVariable)
+   you should see bth are functionse
+  3.create a variable andd assign it
+  to the function call of the variable
+   created in line 2
+   with any radius(number)
+   Console the variable check what you get
+*/
+
+function permiterCirle(radius) {
+  let ans = 2 * radius * 3.142;
+  return ans;
+}
+
+function promptForRadius() {
+  let radius = prompt("Enter your circle radius:");
+  let numRadius = Number(radius);
+
+  if (!numRadius) {
+    //do something
+    alert("Enter a valid number");
+    return;
+  }
+
+  let area = areaCirle(numRadius);
+  let perimter = permiterCirle(numRadius);
+
+  alert(`
+     For a circle with radius ${numRadius}
+     The area is ${area}
+     The permiter is ${perimter}
+    `);
+}
---
+++ JS/juniorDev/scope.js
@@ -0,0 +1,157 @@
+/*
+ It determines accebility
+ and availability of 
+ variables and functions.
+*/
+
+/*
+ 1.Global scop
+ 2.Block scope: {}
+ 3.Lexical Scope
+*/
+
+/*
+ Global scope in variable or function
+ declared outside of any
+ function or  block :{}
+*/
+
+/*
+  for converting usd
+  to ksh
+*/
+
+/*
+  1.Try moving the variable
+  inside the print exhange rate
+  2.Move to the prompt for usd function\
+  3, Move it to the global scope
+*/
+
+/*
+  Block Scope
+  in function or variable that is
+  defined inside  block
+  its only acceble inside that block {}
+  or its children
+
+  example:
+  Print exhange rate function it cant ise variable 
+  of the updateExchangeRate
+
+*/
+
+let usdToKshRate = 130;
+
+function updateExchangeRate() {
+  let newExchangeRate = prompt(
+    `Current exhange rate ${usdToKshRate}. Entery new Exhange rate`
+  );
+  //number
+  let numberExchangeRate = Number(newExchangeRate);
+  if (!numberExchangeRate) {
+    alert(`Enter a number to to change exhange rate`);
+    return;
+  }
+  usdToKshRate = numberExchangeRate;
+}
+
+function printExchangeRate() {
+  alert(`The current exhange rate
+         1 USD = ${usdToKshRate} KSH
+        `);
+}
+
+function promptForUsd() {
+  let amountInUsd = prompt("Enter Amount in usd");
+  //Text or number
+  let amountNumber = Number(amountInUsd);
+
+  if (!amountNumber) {
+    alert("Enter the amount as a number");
+    return;
+  }
+  console.log(`amoun entered is a number ${amountNumber}`);
+  let kshAmount = usdToKshRate * amountNumber;
+
+  alert(`
+     For usd $${amountNumber}
+     Is equal ${kshAmount} KSH
+     Exchange Rate ${usdToKshRate}
+    `);
+}
+
+let myVar1 = 5;
+function blockScope() {
+  let myVar1 = 120;
+
+  if (true) {
+    myVar1 = 3;
+    console.log(myVar1);
+  }
+}
+
+blockScope();
+
+/*
+ 1.Create function name any thing
+  in my case blockScope
+2.Inside that function create 
+a variable mine is myVar1 make
+ it a string of your choice
+3.in the global scope try
+to console the variable.
+Make the observation
+4.Console the variable inside the function
+ then call your function
+5.child scope can access parent scope variables
+Create the same varibale name as twicw
+ name should be the same. but different text value
+ console.log the variable in the global
+ scope
+ console.log the variable in function 
+ block scope
+ 7.Nested block {} inside the function createte
+ an if true or truthy 
+ so that it always executes.
+ try to console the variable that is inside
+ the function my case my var1\
+ 8.Nested Block.
+ parent access the child scope variables
+ inside the if statemnt inside your finction.
+ create another variable in my case myVar2
+ inside the function but outside the 
+ if console the variable then make your observaion. 
+9. 2 or more variables can have the same
+as long there in different scopes.
+Try to create variables in different scopes
+named the same way and console.log them
+*/
+
+/*
+1.Global scope eerything can be accesed by everything
+2.Block scope {} 
+3.Nestd block scope the child can access the parent variables
+4.  A parent cant access variables of a child
+5.You can have the same variable name as long as there in a different scope
+
+*/
+//{}
+
+let x = 45;
+
+
+function theParent() {
+  let k = 10;
+  let x=99
+  if (true) {
+    let x=76
+     let z = 45;
+    if (true) {
+      let z = 45;
+      console.log(k);
+    }
+    console.log(z);
+  }
+   console.log(z);
+}
