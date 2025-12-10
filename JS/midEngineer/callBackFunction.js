7fde752509de41e6ee20bf56f7480d1d63bdd55f
Arthur-codeX <daniel.karanja239@gmail.com>
2025-12-10 09:18:18 +0300

    updated recursive

--- JS/midEngineer/callBackFunction.js
+++ JS/midEngineer/callBackFunction.js
@@ -6,7 +6,8 @@
 */
 
 /*
-  JS Callback Inbuilt . Time manupulation library
+  JS Callback Inbuilt . 
+  Time manupulation library
 
 */
 
@@ -64,12 +65,143 @@
 */
 
 let timeout = 2 * 1000; // 5 seconds
-setTimeout(() => {
-  let name = prompt("Enter your name");
-  alert(`Welcome ${name} to our website`);
-}, timeout);
+// setTimeout(() => {
+//   let name = prompt("Enter your name");
+//   alert(`Welcome ${name} to our website`);
+// }, timeout);
+setTimeout(byeEnglinsh, 200);
 //convert to an anoynomous
 
 /*
  Research setInterval(functon,2000) every 2 seconds
 */
+
+//Greet
+function greet(param1, param2) {
+  console.log("param1 is ", param1);
+  console.log("Type of param1 ", typeof param1);
+
+  console.log("param2 is ", param2);
+  console.log("Type of param2 ", typeof param2);
+
+  alert(`Good morning ${param1}`);
+  param2();
+}
+
+function byeEnglinsh() {
+  alert("Bye Bye !");
+}
+
+greet("Daniel Karanja", byeSpaninsh);
+
+function byeSpaninsh() {
+  alert("Adios Amigo");
+}
+
+function byeItalian() {
+  alert("Ciao");
+}
+
+/*
+ TO DO
+ 1.Create function that takes in 2 params
+   -param1:string
+   - param2:function 
+2.The function alerts us goodming
+   with goodmorning
+   name
+3.Calls the other function
+  for the bye greeting
+  - english,
+  -spaning
+  -italian
+  -Mother Toungue
+*/
+
+/**
+  Cases.
+  1.If you forget to pass anything
+  2.For param 1 pass a string 
+    forget param 2
+  3.For param 1 pass a string
+    Param 2 pass a string
+  4.param1 pass a function
+    param 2 pass function
+  5.param 1 pass a string
+    param 2 pass a funciton 
+
+
+/*
+    
+    Create  a program to do  varius
+    mathematical stuff
+    using 2 numbers
+    -Addition : create funciton
+    -Subtraction: create function
+    -Division : create function
+    */
+
+/*
+  1.Create funciton called calulator.
+   should take 3 inputs
+  -@param1:number 1
+   -@param2 number 2
+   -@param3: <function>
+  2.Console.log all inputs and their types
+  3.Make sure you function
+  is able to do typesafety
+  4.In the calulate function
+    call the @param3<function>
+    pass a and b. Store 
+    the rusult of the
+    function call in a new variable.
+  5.print for opration 
+   <addition:hint,@param3.name>
+   using num1 and num2 
+   the result <result>
+    
+*/
+
+function calulator(a, b, operation) {
+  console.log("a=", a, " typeof a=", typeof a);
+  console.log("b=", b, " typeof b=", typeof b);
+  console.log("operation=", operation, " typeof operation=", typeof operation);
+
+  if (typeof a !== "number") {
+    alert(`For the calculate a must @param1 number`);
+    return;
+  }
+  if (typeof b !== "number") {
+    alert(`For the calculate a must @param2 number`);
+    return;
+  }
+  if (typeof operation !== "function") {
+    alert(`For the calculate a must @param3 function`);
+    return;
+  }
+
+  const result = operation(a, b);
+  alert(`For operation ${operation.name}
+    Using num1=${a} and num2=${b}
+    The result is ${result}
+    `);
+}
+
+// calulator("dsadsa", "dsads", "dsaadsa");
+function addition(a, b) {
+  const sum = a + b;
+  return sum;
+}
+
+let sum = addition(2, 3);
+console.log(sum);
+
+function subtraction(a, b) {
+  const sub = a - b;
+  return sub;
+}
+
+function multiplication(a, b) {
+  const multi = a * b;
+  return multi;
+}
---
+++ JS/midEngineer/recursiveFunction.js
@@ -0,0 +1,89 @@
+/*
+Recursive Function.
+Where you have scenario
+of  a function calling itself
+*/
+
+/*
+   suppsing i tel you
+   to print the
+   statement
+   I am a js enginerr
+   10 times-> n
+
+   1.Create function
+   called aboutMe
+     1.Task print 
+      i am a js engineer
+    2.I want to know the
+      exact time that 
+      the stament was printed.
+    3. Call the function and 
+    see the results
+    4. We need an exit condition
+    so that we stop goig around in circles
+
+*/
+
+function aboutMe(n) {
+  console.log("N is ", n); //n=5,=4
+  if (n == 0) {
+    //Exit
+    return;
+  }
+  console.log("I am a js engineer"); //Task 1
+  let timestamp = new Date(); //Task 2
+  console.log(timestamp.toISOString()); //Task 3
+  let newN = n - 1; //5-1=4,4-1
+  aboutMe(newN); //Task 4
+}
+
+/*
+
+  1.call the aboutMe and dont pass anything
+  2. Call it and pass any number apart
+     from 10
+  3. Call it and pass the number 10
+
+*/
+//Friend greeting
+/*
+//Create a button to call the function
+for the first time.
+  1.Have a function called friendGreeting
+  2.Function does not take any parameters
+  3.Function should prompt for name of friend
+   // enter the name of your firend
+  4.Print<alert> the name of the friend and say hi
+    Hi John Mbugu Have a a nice Day
+ 5. After this the function should call itself
+   for the next friend
+ 6.Exit condition if some types exit or 
+ an empty string then exit function
+
+*/
+
+function friendGreeting() {
+  const friendName = prompt("Enter the name of your friend:");
+
+  if (friendName === "exit" || friendName === "") {
+    return;
+  }
+
+  alert(`Hi ${friendName} Have a nice day`);
+  friendGreeting();
+}
+
+/*
+  1.Create a funciton called
+   factorial
+
+  2.Give a number n it calutes
+   its factorial
+   -return its factorial
+   example if n=5 
+    5*4*3*2*1 =120
+ -> Hint use a recursive 
+  function
+
+*/
